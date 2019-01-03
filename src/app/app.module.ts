2e5401c3da48f894de3bba828bba54c2fab86234
ATMECS\Saloni.Kothari <saloni.kothari@atmecs.com>
2019-01-03 12:11:08 +0530

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+
+var firebaseconfig = {
+  apiKey: "AIzaSyBKZVi9TO-fd1cWPMlaLRveQK_mDXoWiM4",
+  authDomain: "buildingappwithangularfi-25861.firebaseapp.com",
+  databaseURL: "https://buildingappwithangularfi-25861.firebaseio.com",
+  projectId: "buildingappwithangularfi-25861",
+  storageBucket: "buildingappwithangularfi-25861.appspot.com",
+  messagingSenderId: "387785663622"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,AngularFireModule.initializeApp(firebaseconfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
